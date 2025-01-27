febc480ecf1240eaa0600b54c296f9924ee8ee8a
alunoUFU <alunoufu@ufu.br>
2025-01-27 12:53:12 -0300

    refactor jtask

--- src/esp4.cpp
+++ src/esp4.cpp
@@ -1,6 +1,6 @@
 //https://docs.espressif.com/projects/arduino-esp32/en/latest/api/timer.html
 #include "IIKit.h"   // Biblioteca base do framework Arduino, necessária para funções básicas como Serial e delays.
-#include "util/task.h"
+#include "util/jtask.h"
 
 //Funçao de alterar o estado de um led
 void blinkLEDFunc(uint8_t pin) {
@@ -23,23 +23,19 @@ void managerInputFunc(void) {
     IIKit.WSerial.plot("vlR4a20_2", vlR4a20_2);
 }
 
-CounterConfig taskStruct[] = {
-    {0, 500, [](){blinkLEDFunc(def_pin_D1);}},
-    {0, 50, managerInputFunc}
-};
-
 // Configuração inicial do programa
 void setup() {
     // Faz as configuções do hardware ESP + Perifericos
     IIKit.setup();
     pinMode(def_pin_D1, OUTPUT);
-    // Configuração do timer
-    taskSetup(taskStruct,2,1000,1024);
+    jtaskSetup(1000);    // Configura o timer para 1000 Hz (1 ms)
+    jtaskAttachFunc(managerInputFunc, 50); //anexa um função e sua base de tempo para ser executada
+    jtaskAttachFunc([](){blinkLEDFunc(def_pin_D1)}, 500);  //anexa um função e sua base de tempo para ser executada
 }
 
 // Loop principal
 void loop() {
   // Monitora os perifericos
   IIKit.loop();
-  taskLoop();
+  jtaskLoop();
 }
