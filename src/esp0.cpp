f9d0bcc391c40154f650a48ae4463130953da853
josuemoraisgh <josuemorais@gmail.com>
2025-07-03 10:14:14 -0300

    refactor esp0.cpp

--- src/esp0.cpp
+++ src/esp0.cpp
@@ -1,6 +1,12 @@
 #include "IIKitmini.h"
 #include "util/asyncDelay.h"
 
+#define TIME_DELAY_MS1 500
+uint64_t previousTimeMS1 = 0;
+
+#define TIME_DELAY_MS2 50
+uint64_t previousTimeMS2 = 0;
+
 void blinkLEDFunc(uint8_t pin) {
     digitalWrite(pin, !digitalRead(pin));
 }
@@ -19,11 +25,18 @@ void setup()
   pinMode(def_pin_D1, OUTPUT);
 }
 
-AsyncDelay_c blinkLED(500); // time mili second
-AsyncDelay_c delayPOT(50); // time mili second
 void loop()
 {
   IIKit.loop();
-  if (blinkLED.isExpired()) blinkLEDFunc(def_pin_D1);
-  if (delayPOT.isExpired()) managerInputFunc();
+  const uint64_t currentTimeMS = millis();
+  if ((currentTimeMS - previousTimeMS1) >= TIME_DELAY_MS1)
+  {
+    previousTimeMS1 = currentTimeMS;
+    blinkLEDFunc(def_pin_D1);
+  }
+  if ((currentTimeMS - previousTimeMS2) >= TIME_DELAY_MS2)
+  {
+    previousTimeMS2 = currentTimeMS;
+    managerInputFunc();
+  } 
 }
