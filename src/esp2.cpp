87aab66e4751d09e057dabe06a4fb83b87df88ba
alunoUFU <alunoufu@ufu.br>
2025-11-25 10:21:28 -0300

    refactor iikit

--- src/esp2.cpp
+++ src/esp2.cpp
@@ -15,7 +15,7 @@ void managerInputFunc(void) {
     const uint16_t vlPOT2 = IIKit.analogReadPot2();
     IIKit.disp.setText(2, ("P1:" + String(vlPOT1)).c_str());
     IIKit.disp.setText(3, ("P2:" + String(vlPOT2)).c_str());    
-    wserial::WSerial.plot("vlPOT1", vlPOT1);
+    wserial::plot("vlPOT1", vlPOT1);
     wserial::plot("vlPOT2", vlPOT2);
 }
 
